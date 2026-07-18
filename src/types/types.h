402f78b16e004d5eb8632c10c9bf8e7d6b104290
abdullatifhasanarslan <abdullatifhasanarslan@gmail.com>
2026-07-18 22:56:18 +0300

    purge

--- src/types/types.h
+++ src/types/types.h
@@ -10,6 +10,18 @@ enum width
 	DOUBLE
 };
 
+typedef struct dinstr_t{
+	int valid;
+	//opcode;
+	//rs1_valid;
+	//rs1_idx;
+	//rs2_valid;
+	//rs2_idx;
+	//rd_valid;
+	//rd_idx;
+	//imm;
+}
+
 typedef struct packed{
 	bool valid;
 	int32_t data;
